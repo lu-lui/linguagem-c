681ff62f0e0315b0c8dd2be78611c2de42c9d1ea
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-13 15:01:05 -0300

    ex-07

--- lista-2/ex07.c
+++ lista-2/ex07.c
@@ -3,6 +3,27 @@
 #include <stdio.h>
 
 int main(){
+    int numero, resto;
+    printf("Digite um numero inteiro:");
+    scanf("%d", &numero);
 
+    resto = numero % 2;
+
+    if (resto == 0)
+        printf("O numero e par.\n");
+    else
+        printf("O numero e impar.\n");
+
+    if (numero > 0)
+        printf("O numero e positivo.");
+    else
+        if (numero < 0)
+            printf("O numero e negativo.");
+        else
+            printf("O numero e zero.");
+
+    
+    
+    
     
 }
