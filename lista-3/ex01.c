00283b79360d19e8a91d46548655ec4c399dd749
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-15 20:31:45 -0300

    Update ex01.c

--- lista-3/ex01.c
+++ lista-3/ex01.c
@@ -1,11 +1,4 @@
-/*4.1.Escreva um algoritmo para ler as notas da 1a e 2a avaliações de um aluno, calcular a média e escrever se este aluno foi APROVADO, REPROVADO ou se está em EXAME. Escrever também a média calculada. OBS: Para ter direito ao exame o aluno deve obter média mínima 3.0.
-[Entrada]    | [Entrada]    | [Entrada]
-5.0 (nota 1) | 8.0 (nota 1) | 2.0 (nota 1)
-4.0 (nota 2) | 9.0 (nota 2) | 1.0 (nota 2)
-             |              |
-[Saída]      | [Saída]      | [Saída]
-4.5 (média)  | 8.5 (média)  | 1.5 (média)
-em Exame     | Aprovado     | Reprovado*/
+/*4.1.Escreva um algoritmo para ler as notas da 1a e 2a avaliações de um aluno, calcular a média e escrever se este aluno foi APROVADO, REPROVADO ou se está em EXAME. Escrever também a média calculada. OBS: Para ter direito ao exame o aluno deve obter média mínima 3.0.*/
 
 #include <stdio.h>
 
