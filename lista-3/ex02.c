a1d9d0b755dc1d83cffd481b9256730ed4beb580
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-16 13:50:27 -0300

    ex02

--- lista-3/ex02.c
+++ lista-3/ex02.c
@@ -1,7 +1,23 @@
-/*4.2.Escreva um algoritmo para ler o número de gols marcados pelo Grêmio e o número de gols marcados pelo Inter em um GRENAL. Escrever o nome do vencedor. Caso não haja vencedor deverá ser impresso a palavra EMPATE.
-[Entrada]          | [Entrada]          | [Entrada]
-4 (gols do Grêmio) | 2 (gols do Grêmio) | 0 (gols do Grêmio)
-2 (gols do Inter)  | 2 (gols do Inter)  | 1 (gols do Inter)
-                   |                    |
-[Saída]            | [Saída]            | [Saída]
-Grêmio             | Empate             | Inter*/
+/*4.2.Escreva um algoritmo para ler o número de gols marcados pelo Grêmio e o número de gols marcados pelo Inter em um GRENAL. Escrever o nome do vencedor. Caso não haja vencedor deverá ser impresso a palavra EMPATE.*/
+
+#include <stdio.h>
+
+int main(){
+    int Inter, Gremio;
+
+    printf("Resultado do GreNal:\nGols do Inter: ");
+    scanf("%d", &Inter);
+
+    printf("Gols do Gremio: ");
+    scanf("%d", &Gremio);
+
+    printf("Placar: Inter %d x %d Gremio\n", Inter, Gremio);
+
+    if (Inter > Gremio)
+        printf("Vencedor: INTERNACIONAL");
+    else
+        if (Inter < Gremio)
+            printf("Vencedor: GREMIO");
+        else
+            printf("EMPATE");
+}
