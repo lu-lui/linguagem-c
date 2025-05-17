a52e66064ed3f9d09081a26b9e4e7370b03cae93
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-17 11:44:23 -0300

    ex 03

--- lista-3/ex03.c
+++ lista-3/ex03.c
@@ -2,11 +2,30 @@
 Se o número de lados for igual a 3 escrever TRIÂNGULO e o valor do seu perímetro.
 Se o número de lados for igual a 4 escrever QUADRADO e o valor da sua área.
 Se o número de lados for igual a 5 escrever PENTÁGONO.
-OBS: Considere que o usuário só informará os valores 3,4 ou 5.
-[Entrada]           | [Entrada] | [Entrada]
-3 (número de lados) | 4 (número de lados) | 5 (número de lados)
-2 (medida do lado)  | 5 (medida do lado)  |
-                    |                     |
-[Saída]             | [Saída]             | [Saída]
-Triângulo           | Quadrado            | Pentágono
-6 (perímetro)       | 25 (área)           |*/
+OBS: Considere que o usuário só informará os valores 3,4 ou 5.*/
+
+#include <stdio.h>
+
+int main(){
+    int numeroLados;
+    float medidaLados, area, perimetro;
+
+    printf("Numero de lados do poligono REGULAR(3, 4 ou 5): ");
+    scanf("%d", &numeroLados);
+
+    if (numeroLados == 5)
+        printf("O poligono e um pentagono");
+    else
+        if (numeroLados == 4){
+            printf("Medida dos lados: ");
+            scanf("%f", &medidaLados);
+            area = medidaLados * medidaLados;
+            printf("O poligono e um quadrado de area %.1f", area);
+        }
+        else {
+            printf("Medida dos lados: ");
+            scanf("%f", &medidaLados);
+            perimetro = medidaLados * 3;
+            printf("O poligono e um triangulo de perimetro %.1f", perimetro); 
+        }
+    }
