1c3f2d56947b35e94d7a5f7cae1700a2f8d40fb8
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-17 12:54:27 -0300

    ex 05

--- lista-3/ex05.c
+++ lista-3/ex05.c
@@ -1,8 +1,33 @@
-/*4.5.Escreva um algoritmo para ler 2 valores e uma das seguintes operações a serem executadas (codificada da seguinte forma: 1.Adição, 2.Subtração, 3.Divisão, 4 Multiplicação). Calcular e escrever o resultado dessa operação sobre os dois valores lidos.
-[Entrada]     | [Entrada]     | [Entrada]       | [Entrada]
-5             | 10            | 7               | 12
-3             | 3             | 2               | 5
-1 (adição)    | 2 (subtração) | 3 (divisão)     | 4 (multiplicação)
-              |               |                 |
-[Saída]       |   [Saída]     | [Saída]         | [Saída]
-8 (resultado) | 7 (resultado) | 3.5 (resultado) | 60 (resultado)*/
+/*4.5.Escreva um algoritmo para ler 2 valores e uma das seguintes operações a serem executadas (codificada da seguinte forma: 1.Adição, 2.Subtração, 3.Divisão, 4 Multiplicação). Calcular e escrever o resultado dessa operação sobre os dois valores lidos.*/
+
+#include <stdio.h>
+
+int main (){
+    float valor1, valor2, resultado;
+    int calculo;
+    
+    printf("Escreva dois valores:\nValor 1: ");
+    scanf("%f", &valor1);
+    printf("Valor 2: ");
+    scanf("%f", &valor2);
+    printf("Escolha uma opcao:\n1 - Adicao;\n2 - Subtracao;\n3 - Divisao;\n4 - Multiplicacao.\n");
+    scanf("%d", &calculo);
+
+    if (calculo == 1){
+        resultado = valor1 + valor2;
+        printf("Total: %.1f", resultado);
+    } else 
+        if (calculo == 2){
+            resultado = valor1 - valor2;
+            printf("Resultado: %.1f", resultado);
+        } else
+            if (calculo == 3){
+                resultado = valor1 / valor2;
+                printf("Resultado: %.1f", resultado);
+            } else
+                if (calculo == 4){
+                    resultado = valor1 * valor2;
+                    printf("Resultado: %.1f", resultado);
+                } else
+                    printf("Nao e uma opcao aceita");
+}
