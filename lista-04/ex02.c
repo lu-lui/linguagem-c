d239d3a41fed59506e2bc52f4f5730d9703faff4
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-26 16:23:06 -0300

    ex02

--- lista-04/ex02.c
+++ lista-04/ex02.c
@@ -1,2 +1,17 @@
-/*2. Faça um algoritmo que gera e escreve os números ímpares entre 100 e 200.
-Utilize a função resto para verificar se o número é impar.*/
+/*2. Faça um algoritmo que gera e escreve os números ímpares entre 100 e 200. Utilize a função resto para verificar se o número é impar.*/
+
+//Função resto ex: % 2, resto da divisao por 2
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+
+    for (numero = 100; numero <= 200; numero++){
+        if (numero % 2 != 0)
+            printf("%d\n", numero);
+    }
+
+}
+
+   
