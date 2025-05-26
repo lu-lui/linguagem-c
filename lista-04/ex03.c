18c99047564f968189fb9a0712ff66ba75263594
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-05-26 17:14:19 -0300

    ok

--- lista-04/ex03.c
+++ lista-04/ex03.c
@@ -1,1 +1,18 @@
 /*3. Faça um algoritmo para calcular o fatorial de um número.*/
+
+#include <stdio.h>
+
+int main(){
+
+    int numero, n;
+
+    printf("Digite um numero: ");
+    scanf("%d", numero);
+
+    n = numero; 
+    for(n = numero; n = 1; n--){
+        n * n;
+    printf("O fatorial de %d e %d", numero, n);
+    }
+}
+//fazer dps
--- lista-04/ex04.c
+++ lista-04/ex04.c
@@ -1,2 +1,21 @@
 /*4. Ler um valor inteiro (aceitar somente valores entre 1 e 10) e escrever a tabuada
 de 10 a 1 do valor lido.*/
+
+#include <stdio.h>
+
+int main(){
+    int valor, multiplicador;
+
+    printf("Digite um valor inteiro de 1 a 10: ");
+    scanf("%d", &valor);
+
+    while (valor < 1 || valor > 10){
+        printf("Valor invalido.\nDigite novamente:");
+        scanf("%d", &valor);
+    }
+
+    for(multiplicador = 10; multiplicador >= 1; multiplicador--){
+        printf("%d X %d = %d\n", valor, multiplicador, valor * multiplicador);
+
+    }
+}
