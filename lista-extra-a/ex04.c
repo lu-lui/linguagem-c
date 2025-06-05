458e9223b7edcab8509758cf370f2ae2e60d1721
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-05 18:22:03 -0300

    Update ex04.c

--- lista-extra-a/ex04.c
+++ lista-extra-a/ex04.c
@@ -1,2 +1,22 @@
-/*8.4.Altere a solução do exercício 8.3 para que seja impressa a mensagem Valor inválido caso o segundo valor informado seja 0.
-*/
+/*8.4.Altere a solução do exercício 8.3 para que seja impressa a mensagem Valor inválido caso o segundo valor informado seja 0. */
+
+#include <stdio.h>
+
+int main(){
+    int v1, v2;
+    float divisao;
+    
+    do {
+        printf("Digite um dividendo: ");
+        scanf("%d", &v1);
+        printf("Digite um divisor: ");
+        scanf("%d", &v2); 
+
+        if(v2 == 0)
+        printf("Valor invalido, tente novamente com um divisor diferente de 0!");
+
+        divisao = v1 / v2;
+    } while (v2 == 0);
+
+    printf("O resultado da divisao de %d por %d e %.2f", v1, v2, divisao);
+} // Já tinha feito isso no exercício 3 :)
