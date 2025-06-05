4e395728dd28898587959c283e82d995ba14b9f1
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-05 18:45:48 -0300

    ok

--- lista-extra-a/ex06.c
+++ lista-extra-a/ex06.c
@@ -1,14 +1,35 @@
-/*8.6.Reescreva o algoritmo para o problema 8.5 para que no final seja impressa a mensagem Novo cálculo (1.sim 2.não) solicitando ao usuário que informe um código (1 ou 2) indicando se ele deseja ou não executar o algoritmo novamente. Se for informado o código 1 deve ser repetida a execução de todo o algoritmo para permitir um novo cálculo, caso contrário ele deve ser encerrado.
-[Entrada] [Saída]
--2 (nota 1) Nota inválida
-7 (nota 1)
-9 (nota 2) 8 (média)
- Novo cálculo (1.sim 2.não)?
-1
-6 (nota 1)
-12 (nota 2) Nota inválida
--3 (nota 2) Nota inválida
-10 (nota 2) 8 (média)
- Novo cálculo (1.sim 2.não)?
-2
-*/
+/*8.6.Reescreva o algoritmo para o problema 8.5 para que no final seja impressa a mensagem Novo cálculo (1.sim 2.não) solicitando ao usuário que informe um código (1 ou 2) indicando se ele deseja ou não executar o algoritmo novamente. Se for informado o código 1 deve ser repetida a execução de todo o algoritmo para permitir um novo cálculo, caso contrário ele deve ser encerrado.*/
+
+#include <stdio.h>
+
+int main(){
+    
+    float nota1, nota2, media;
+    int resposta;
+
+    do{
+        printf("Nota 1: ");
+        scanf("%f", &nota1);
+
+        while (nota1 <= 0 || nota1 > 10)
+        {
+            printf("Nota invalida\n");
+            scanf("%f", &nota1);
+        }
+        
+        printf("Nota 2: ");
+        scanf("%f", &nota2);
+
+        while (nota2 <= 0 || nota2 > 10)
+        {
+            printf("Nota invalida\n");
+            scanf("%f", &nota2);
+        }
+
+        media = (nota1 + nota2) / 2;
+
+        printf("Media do aluno: %.1f\nDeseja fazer o calculo novamente?\n 1.Sim 2.Nao\n", media);
+        scanf("%d", &resposta);
+
+    }while(resposta == 1);
+}
--- lista-extra-a/ex07.c
+++ lista-extra-a/ex07.c
@@ -1,19 +1,41 @@
 /*8.7.Reescreva o algoritmo do exercício 8.6 validando a resposta do usuário para a pergunta Novo Cálculo
-(1.sim 2.não)? (aceitar apenas os código 1 ou 2).
-[Entrada] [Saída]
--2 (nota 1) Nota inválida
-7 (nota 1)
-9 (nota 2) 8 (média)
- Novo cálculo (1.sim 2.não)?
-3
- Novo cálculo (1.sim 2.não)?
-0
- Novo cálculo (1.sim 2.não)?
-1
-6 (nota 1)
-12 (nota 2) Nota inválida
--3 (nota 2) Nota inválida
-10 (nota 2) 8 (média)
- Novo cálculo (1.sim 2.não)?
-2
-*/
+(1.sim 2.não)? (aceitar apenas os código 1 ou 2).*/
+
+#include <stdio.h>
+
+int main(){
+    
+    float nota1, nota2, media;
+    int resposta;
+
+    do{
+        printf("Nota 1: ");
+        scanf("%f", &nota1);
+
+        while (nota1 <= 0 || nota1 > 10)
+        {
+            printf("Nota invalida\n");
+            scanf("%f", &nota1);
+        }
+        
+        printf("Nota 2: ");
+        scanf("%f", &nota2);
+
+        while (nota2 <= 0 || nota2 > 10)
+        {
+            printf("Nota invalida\n");
+            scanf("%f", &nota2);
+        }
+
+        media = (nota1 + nota2) / 2;
+
+        printf("Media do aluno: %.1f\nDeseja fazer o calculo novamente?\n 1.Sim 2.Nao\n", media);
+        scanf("%d", &resposta);
+
+        while (resposta != 1 && resposta != 2){
+            printf("Resposta invalida, digite novamente:\n");
+            scanf("%d", &resposta);
+        }
+        
+    }while(resposta == 1);
+}
