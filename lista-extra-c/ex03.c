1faa2418396b754ced23665ec2d0a8cdb47ab030
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-09 13:47:17 -0300

    ok

--- lista-extra-c/ex03.c
+++ lista-extra-c/ex03.c
@@ -1,5 +1,18 @@
-/*11.3 Ler 10 valores, calcular e escrever a média aritmética destes valores.
-[Entrada] [Saída]
-10 3 12 5 25 0 13 60 70 2
- 20 (média)
-*/
+/*11.3 Ler 10 valores, calcular e escrever a média aritmética destes valores.*/
+
+#include <stdio.h>
+
+int main(){
+    float valor, somavalor = 0, media;
+
+    for (int i = 1; i <= 10; i++){
+        printf("Digite um valor: ");
+        scanf("%f", &valor);
+
+        somavalor = somavalor + valor;
+    }
+    
+    media = somavalor / 10;
+
+    printf("A media dos valores digitados e: %.2f", media);
+}
