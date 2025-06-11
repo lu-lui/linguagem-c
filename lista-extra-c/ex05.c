b1ca6a59207823a472ebc5fe8e47293b9bb3ff7a
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-11 17:50:19 -0300

    ok

--- lista-extra-c/ex05.c
+++ lista-extra-c/ex05.c
@@ -1,6 +1,24 @@
-/*11.5 Ler 2 valores (considere que o primeiro valor lido sempre será menor que o segundo), calcular e escrever a soma dos números inteiros existentes entre eles(incluindo os 2 valores lidos na soma).
-[Entrada] [Saída]
-3 (primeiro)
-7 (segundo)
- 25 (soma)
-*/
+/*11.5 Ler 2 valores (considere que o primeiro valor lido sempre será menor que o segundo), calcular e escrever a soma dos números inteiros existentes entre eles(incluindo os 2 valores lidos na soma).*/
+
+#include <stdio.h>
+
+int main(){
+    int v1, v2, soma = 0;
+
+    printf("Digite dois valores diferentes, em ordem crescente: \nValor 1: ");
+    scanf("%d", &v1);
+
+    printf("Valor 2: ");
+    scanf("%d", &v2);
+
+    while (v2 <= v1){
+        printf("Valor invalido, digite um valor maior que %d: ", v1);
+        scanf("%d", &v2);
+    }
+    
+    for ( int i = v1; i <= v2; i++){
+        soma = soma + v1;
+        v1++;
+    }
+    printf("A soma dos numeros entre os numeros digitados (com ambos inclusos) e: %d", soma);
+}
