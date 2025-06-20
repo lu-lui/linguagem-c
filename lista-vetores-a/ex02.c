029926cf58bb61043d1d89d36a27257186a3494f
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-20 13:58:46 -0300

    ex02

--- lista-vetores-a/ex02.c
+++ lista-vetores-a/ex02.c
@@ -1,9 +1,14 @@
-/*14.2 Preencher um vetor A de 10 elementos(índices de 0 a 9) com os números inteiros 10,20,30,40,50,...,100. Escrever o vetor A após o seu total preenchimento.
-[Vetor A após o preenchimento]
- 0 1 2 3 4 5 6 7 8 9
-+----+----+----+----+----+----+----+----+----+----+
-| 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 |100 |
-+----+----+----+----+----+----+----+----+----+----+
-[Saída]
-10 20 30 40 50 60 70 80 90 100
-*/
+/*14.2 Preencher um vetor A de 10 elementos(índices de 0 a 9) com os números inteiros 10,20,30,40,50,...,100. Escrever o vetor A após o seu total preenchimento.*/
+
+#include <stdio.h>
+
+int main(){
+
+    int A[10], i;
+
+    for (i = 0; i < 10; i++){
+        A[i] = (i+1) * 10;
+        
+       printf("%d ", A[i]);
+    }  
+}
