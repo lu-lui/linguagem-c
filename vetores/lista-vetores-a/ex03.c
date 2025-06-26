89b55160d05d6e767caa89cb1178f839821acaf1
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-26 14:50:34 -0300

    ok

--- vetores/lista-vetores-a/ex03.c
+++ vetores/lista-vetores-a/ex03.c
@@ -1,8 +1,20 @@
-/*14.3 Preencher um vetor B de 10 elementos com 10 se o índice do elemento for ímpar, e 20 se for par. Escrever o vetor B após o seu total preenchimento.
-[Vetor B após o preenchimento]
- 0 1 2 3 4 5 6 7 8 9
-+----+----+----+----+----+----+----+----+----+----+
-| 20 | 10 | 20 | 10 | 20 | 10 | 20 | 10 | 20 | 10 |
-+----+----+----+----+----+----+----+----+----+----+
-[Saída]
-20 10 20 10 20 10 20 10 20 10*/
+/*14.3 Preencher um vetor B de 10 elementos com 10 se o índice do elemento for ímpar, e 20 se for par. Escrever o vetor B após o seu total preenchimento.*/
+
+#include <stdio.h>
+
+int main(){
+    int B[10], x;
+
+   for(int i=0; i<10; i++){
+        scanf("%d", &x);
+       
+        if(x % 2 == 0)
+            B[i] = 20;
+        else 
+             B[i] = 10;
+   } 
+
+    for(int i=0; i<10; i++){
+        printf("%d ", B[i]);
+    }
+}
--- vetores/lista-vetores-a/ex04.c
+++ vetores/lista-vetores-a/ex04.c
@@ -1,13 +1,21 @@
-/*14.4 Ler um vetor C de 10 elementos inteiros. Após o término da leitura, trocar todos os valores negativos do vetor C por 0. Após todas as trocas terem sido efetuadas, escrever o vetor C.
-[Entrada: Vetor C]
- 0 1 2 3 4 5 6 7 8 9
-+----+----+----+----+----+----+----+----+----+----+
-| 21 | 13 | -2 | -1 | 12 | -4 | -5 | 42 | 0 | 32 |
-+----+----+----+----+----+----+----+----+----+----+
-[Vetor C após as trocas]
- 0 1 2 3 4 5 6 7 8 9
-+----+----+----+----+----+----+----+----+----+----+
-| 21 | 13 | 0 | 0 | 12 | 0 | 0 | 42 | 0 | 32 |
-+----+----+----+----+----+----+----+----+----+----+
-[Saída]
-21 13 0 0 12 0 0 42 0 32*/
+/*14.4 Ler um vetor C de 10 elementos inteiros. Após o término da leitura, trocar todos os valores negativos do vetor C por 0. Após todas as trocas terem sido efetuadas, escrever o vetor C.*/
+
+#include <stdio.h>
+
+int main(){
+    int C[10], x;
+
+    for (int i = 0; i < 10; i++){
+        scanf("%d", &x);
+
+        if(x < 0)
+            C[i] = 0;
+        else 
+            C[i] = x;  
+    }
+
+    for(int i=0; i<10; i++){
+        printf("%d ", C[i]);
+    }
+    
+}
