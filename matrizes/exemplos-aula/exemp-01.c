6b4868336bcb0de2f66a33a0b9a61efb58e9792a
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-06-25 23:00:08 -0300

    ok

---
+++ matrizes/exemplos-aula/exemp-01.c
@@ -0,0 +1,18 @@
+/*Algoritmo para escrever uma matriz 3x3 e preencher tdo com 0.*/
+
+#include <stdio.h>
+
+int main(){
+
+    int m[3][3];
+
+    for(int l=0; l<3; l++){
+        for(int c=0; c<3; c++){
+            m[l][c] = 0;
+            
+             printf("%d", m[l][c]);
+        }
+       printf("\n");
+    }
+
+}
