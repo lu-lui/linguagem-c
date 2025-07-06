db29e2cc6ff17229771f7326b8089abc2fa020aa
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-07-06 00:53:57 -0300

    ok

--- vetores/lista-vetores-b/ex02.c
+++ vetores/lista-vetores-b/ex02.c
@@ -1,6 +1,28 @@
-/*15.2 Ler um vetor R de 5 elementos contendo o gabarito da LOTO. A seguir ler um vetor A de 10 elementos contendo uma aposta. A seguir imprima quantos pontos fez o apostador.
-[Entrada]
-4 12 34 25 17 (gabarito)
-3 17 55 21 34 4 27 29 20 11 (aposta)
-[Saída]
-3 (pontos)*/
+/*15.2 Ler um vetor R de 5 elementos contendo o gabarito da LOTO. A seguir ler um vetor A de 10 elementos contendo uma aposta. A seguir imprima quantos pontos fez o apostador.*/
+
+#include <stdio.h>
+
+int main(){
+    int R[5], A[10], c=0;
+
+    printf("LOTO \n Gabarito: \n");
+
+    for (int i = 0; i < 5; i++){
+        scanf("%d", &R[i]);
+    }
+
+    printf("Aposta do jogador: \n");
+    for (int i = 0; i < 10; i++){
+        scanf("%d", &A[i]);
+
+    }
+
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 5; j++){
+            if (R[j] == A[i])
+                c++;
+        }
+    }
+
+    printf("O apostador fez %d pontos", c);
+}
