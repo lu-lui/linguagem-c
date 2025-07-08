53b7c20815232a42a847881bc5cb948c5acc1529
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-07-08 16:02:42 -0300

    ok

--- matrizes/ex-matrizes-l/ex05.c
+++ matrizes/ex-matrizes-l/ex05.c
@@ -1,22 +1,31 @@
 /*16.5.Ler uma matriz G 5 x 5 e criar 2 vetores SL e SC de 5 elementos que contenham respectivamente as somas das linhas e das colunas de G. Escrever os vetores criados.*/
 
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main(){
-    int G[3][3], SL[3], SC[3];
+    int G[5][5], SL[5] = {0}, SC[5] = {0};
 
     printf("Elementos da matriz G: \n");
-    for (int l = 0; l < 3; l++){
-        for (int c = 0; c < 3; c++){
+    for (int l = 0; l < 5; l++){
+        for (int c = 0; c < 5; c++){
             scanf("%d", &G[l][c]);
         }
     }
     
-    for (int l = 0; l < 3; l++){
-        for (int c = l; c < 3; c++){
-             SL[l] = G[l][c];
-             printf("%d ", &SL[l]);
+    for (int l = 0; l < 5; l++){
+        for (int c = 0; c < 5; c++){
+            SL[l] += G[l][c];
+            SC[c] += G[l][c];
         }
     }
+
+    printf("Somas das linhas de G: ");
+    for (int i = 0; i < 5; i++){
+        printf("%d ", SL[i]);
+    }
     
-}*/
+    printf("\nSomas das colunas de G: ");
+    for (int i = 0; i < 5; i++){
+         printf("%d ", SC[i]);
+    }
+}
--- matrizes/ex-matrizes-l/ex06.c
+++ matrizes/ex-matrizes-l/ex06.c
@@ -1,9 +1,34 @@
 /*16.6.Ler uma matriz A 12 x 13 e divida todos os 13 elementos de cada uma das 12 linhas de A pelo valor do maior elemento daquela linha. Escrever a matriz A modificada.*/
 
-/*
+
+/*16.6.Ler uma matriz A 2 x 3 e divida todos os 3 elementos de cada uma das 2 linhas de A pelo valor do maior elemento daquela linha. Escrever a matriz A modificada.*/
+
 #include <stdio.h>
 
 int main(){
-    int A[12][13];
+    float A[12][13], X[2] = {-1000} ;
+
+    printf("Matriz A: \n");
+    for (int l = 0; l < 12; l++){
+        for (int c = 0; c < 13; c++){
+            scanf("%f", &A[l][c]);
+            if (A[l][c] > X[l])
+                X[l] = A[l][c];
+            
+        }
+    }
+
+    for (int l = 0; l < 12; l++){
+        for (int c = 0; c < 13; c++){
+           A[l][c] = A[l][c] / X[l];
+        }
+    }
+
+    printf("Matriz A modificada: \n");
+    for (int l = 0; l < 12; l++){
+        for (int c = 0; c < 13; c++){
+           printf("%.2f ", A[l][c]);
+        }
+        printf("\n");
+    }
 }
-*/
