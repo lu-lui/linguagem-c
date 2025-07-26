dc8a7efc6e03c58aa217a769a08356f84234c956
lu-lui <197313513+lu-lui@users.noreply.github.com>
2025-07-26 17:25:02 -0300

    Update ex03.c

--- structs/lista-1/ex03.c
+++ structs/lista-1/ex03.c
@@ -1,1 +1,7 @@
 /*4) Escreva um programa que receba dois structs do tipo dma, cada um representando uma data válida, e calcule o número de dias que decorreram entre as duas datas. OBS: Garanta que a estrutura só armazenará datas válidas.*/
+
+struct dma{
+    int dia;
+    int mes;
+    int ano; 
+};
